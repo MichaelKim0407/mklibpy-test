129545eeb76cec3dacdd8fb6228e75869fd0115d
Michael Kim <mkim0407@gmail.com>
2016-10-03 17:51:36 +0800

    MultiWriter

---
+++ multiwriter_test.py
@@ -0,0 +1,14 @@
+from mklibpy.file.out import MultiWriter
+
+__author__ = 'Michael'
+
+
+with MultiWriter(True, "multiwriter.out") as writer:
+    writer.writeline("Hello world!")
+    writer.writeline("This is {} speaking.", "captain")
+    writer.writelines("1", "2", "3")
+
+
+writer.add_file("mw2.out", "a")
+with writer:
+    writer.writeline("I'm back!")
